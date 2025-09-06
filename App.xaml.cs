b3647b3cadb46e758956d3c8f8025e5a72269000
karthiksai-m <karthiksai1103@gmail.com>
2025-09-06 20:25:14 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,26 @@
+using System;
+using System.Threading;
 using System.Windows;
 
 namespace DesktopClockWidget
 {
-    public partial class App : Application { }
+    public partial class App : Application
+    {
+        private static Mutex mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createdNew;
+            mutex = new Mutex(true, "DesktopClockWidget_Mutex", out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("Program already running.", "Desktop Clock Widget");
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+    }
 }
